416f33fb4e71728700e0574fef2d1f28fbc492f8
Ruben Hales <rh.2022.tech@gmail.com>
2023-07-28 15:08:57 -0400

    Initial Commit

---
+++ news_app3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news_app3
+//
+//  Created by Ruben Hales on 7/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
